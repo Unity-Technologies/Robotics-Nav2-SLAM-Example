bf00eb9daaac736994b2b218f5b79f8f46b37ea3
Devin Miller (Unity) <mrpropellers@users.noreply.github.com>
2021-08-18 15:28:31 -0700

    Correcting start_ros script

--- .yamato/start-ros.bash
+++ .yamato/start-ros.bash
@@ -5,7 +5,8 @@ set -e
 # Assuming this script is invoked from the root of the repository...
 DIR_ORIGINAL=$PWD
 cd ros2_docker/colcon_ws
-colcon install
+colcon build
 source install/local_setup.bash
 cd "$DIR_ORIGINAL"
+# TODO: Expose a parameter to disable RViz since we don't need it here
 ros2 launch unity_slam_example unity_slam_example.py &
